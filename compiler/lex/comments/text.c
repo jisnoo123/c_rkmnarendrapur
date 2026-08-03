8e8e845d9dbfd9656e848e7b88bfa117f4b7e516
jisnoo123 <jisnoodevpal@gmail.com>
2026-08-03 21:38:00 +0530

    Add more lex programs

---
+++ compiler/lex/comments/text.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// Sample SLC 1
+int main(){
+	int i=0; //Variable
+	
+	/* Loop is running
+	 to print "Hi" ten times */
+
+	for(; i<10; i++){
+		printf("Hi");
+	}
+	// end of for
+}
